c8ec8328bc1e855036eda0ffc6d81421fcc30e6a
root <M-Umwari>
2022-11-04 05:38:24 -0700

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,6 @@
-list
-    elif idx >= len(my_list):
-        return my_list
-    else:
-        new_list = list(my_list)
-        new_list[idx] = element
-        return new_list
-
+#!/usr/bin/python3
+def no_c(my_string):
+    my_string = my_string.translate({ord("c"): None})
+    my_string = my_string.translate({ord("C"): None})
+    return my_string
 
