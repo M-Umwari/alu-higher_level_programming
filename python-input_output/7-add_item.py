5091c320759053721b0f2470700114833e9c4d11
root <M-Umwari>
2022-11-18 04:14:05 -0800

    5-save_to_json_file.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""
-Module 7-add_item.py
-"""
+"""Add all arguments to a Python list and save them to a file."""
+import sys
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-f_name = "add_item.json"
-
-try:
-    data = load_from_json_file(f_name)
-except FileNotFoundError:
-    data = []
-
-save_to_json_file(data + argv[1:], f_name)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
