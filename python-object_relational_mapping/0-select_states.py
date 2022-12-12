2e033f7ae6d874e00969408d63624c70b0620286
M-Umwari <m.umwari@alustudent.com>
2022-12-12 14:45:23 +0200

    relational mapping

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 
-""" This script  lists all states from the database hbtn_0e_0_usa"""
+""" A script that lists all states from the database"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    get_db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],password=argv[2], db=argv[3])
+    db_get = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        charset="utf8")
 
-    opra = get_db.cursor()
-    opra.execute("SELECT * FROM states ORDER BY id ASC")
-    for row in opra.fetchall():
+    ora = db_get.cursor()
+    ora.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for row in ora.fetchall():
         print(row)
-    opra.close()
-    get_db.close()
+    ora.close()
+    db_get.close()
