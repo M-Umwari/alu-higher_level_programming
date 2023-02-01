984e91710b007659b3e3311302367fd6a850494d
M-Umwari <m.umwari@alustudent.com>
2023-02-01 13:04:26 +0200

    languages

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-let myVar = 'C is fun';
-let myvariable = 'Python is cool';
-let varmy ='JavaScript is amazing';
-console.log(myVar);
-console.log(myvariable);
-console.log(varmy);
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
+
